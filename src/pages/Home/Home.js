dedabc8a658e04bea6818d1e23b36ba600addf56
Pierre-Etienne <pierre-etienne@MacBook-Pro-de-Pierre-Etienne.local>
2022-11-01 21:59:00 +0100

    banner

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,16 @@
-import "./style.scss";
 import Header from "../../components/Header/Header";
+import Banner from "../../assets/images/banner.jpg";
+
+import "./style.scss";
 
 const Home = () => {
   return (
-    <div>
+    <div className="Home">
       <Header />
+      <div className="banner">
+        <img alt="banner" src={Banner} />
+        <div className="sell-adv container"></div>
+      </div>
     </div>
   );
 };
