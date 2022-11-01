9c20fd68b273d4b71978574b223ac4806cc2d5b3
Pierre-Etienne <pierre-etienne@MacBook-Pro-de-Pierre-Etienne.local>
2022-11-01 22:33:58 +0100

    banner done

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,7 +9,16 @@ const Home = () => {
       <Header />
       <div className="banner">
         <img alt="banner" src={Banner} />
-        <div className="sell-adv container"></div>
+        <div className="sell-adv container">
+          <div className="adv">
+            <div className="content">
+              <h1 className="title-adv">
+                Prêts à faire du tri dans vos placards
+              </h1>
+              <button className="button-adv">Vends maintenant</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
